27e15d4b4850998119646a052b28f619afd31c8b
David Bierce <david@bierce.org>
2026-07-28 17:18:25 -0500

    configure setuptools_scm version resolution for PyPI packaging

--- src/saltext/opnsense/version/__init__.py
+++ src/saltext/opnsense/version/__init__.py
@@ -1,6 +1,10 @@
-from importlib.metadata import PackageNotFoundError, version
-
 try:
-    __version__ = version("saltext-opnsense")
-except PackageNotFoundError:
-    __version__ = "0.0.0.dev+unknown"
+    from saltext.opnsense.version._version import __version__
+except ImportError:
+    try:
+        from importlib.metadata import PackageNotFoundError, version
+
+        __version__ = version("saltext-opnsense")
+    except PackageNotFoundError:
+        __version__ = "0.0.0.dev+unknown"
+
